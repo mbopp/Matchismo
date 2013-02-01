77d8580a5ee1748317f8761da925f406465deadb
Michael Bopp <mbopp@rapiddg.com>
2013-02-01 12:30:46 -0500

    Initial Commit

---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Michael Bopp on 2/1/13.
+//  Copyright (c) 2013 Michael Bopp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mwbAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([mwbAppDelegate class]));
+    }
+}
---
+++ Matchismo/mwbViewController.h
@@ -0,0 +1,13 @@
+//
+//  mwbViewController.h
+//  Matchismo
+//
+//  Created by Michael Bopp on 2/1/13.
+//  Copyright (c) 2013 Michael Bopp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mwbViewController : UIViewController
+
+@end
