83b319dc8823f14ededf08bd68c646d449ba54d3
Michael Bopp <mbopp@rapiddg.com>
2013-02-01 12:49:19 -0500

    rename classes

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Michael Bopp on 2/1/13.
+//  Copyright (c) 2013 Michael Bopp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
