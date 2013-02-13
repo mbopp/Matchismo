c1fbc5144b68a32f0268720eb3e71021cb5a4c1d
Michael Bopp <mbopp@rapiddg.com>
2013-02-13 12:40:52 -0500

    New View Controller for Results Page

---
+++ Matchismo/GameResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultsViewController.h
+//  Matchismo
+//
+//  Created by Michael Bopp on 2/13/13.
+//  Copyright (c) 2013 Michael Bopp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultsViewController : UIViewController
+
+@end
